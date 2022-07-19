47bee1dc40103116e22f10b9b0320ceab00ec84c
Gnarll <vadimfedorovich22@mail.ru>
2022-07-19 22:42:46 +0300

    add rewind on double tap

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,32 +1,72 @@
-import React, { useRef } from 'react'
-import { Dimensions, StyleSheet, View } from 'react-native'
+import React, { useCallback, useRef, useState } from 'react'
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native'
 import Video from 'react-native-video'
 
 const { width: screenWidth } = Dimensions.get('screen')
 
+const videoWidth = screenWidth * 0.9
+const videoHeight = 200
+
+const DOUBLE_PRESS_DELAY = 1000
+
 export const VideoPlayer = ({ uri }: { uri: string }) => {
   const playerRef = useRef<Video | null>(null)
+  const [progress, setProgress] = useState(0)
+
+  const [lastPressed, setLastPressed] = useState(0)
+
+  const rewindLeft = () => playerRef?.current?.seek(progress - 10)
+  const rewindRight = () => playerRef?.current?.seek(progress + 10)
+
+  const rewindOnDoubleTap = useCallback(
+    (locationX: number, locationY: number) => {
+      const time = new Date().getTime()
+
+      const delta = time - lastPressed
+      setLastPressed(time)
+      if (lastPressed && delta < DOUBLE_PRESS_DELAY) {
+        locationX < videoWidth / 2 ? rewindLeft() : rewindRight()
+      }
+    },
+    [lastPressed]
+  )
 
   return (
-    <View style={styles.container}>
-      <Video
-        ref={playerRef}
-        source={{ uri: uri }}
-        onBuffer={() => console.log(1)}
-        onError={() => console.log(1)}
-        style={styles.player}
-        repeat
-        controls
-        rate={1}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={{ width: '100%', height: '100%', zIndex: 10 }}
+        activeOpacity={1}
+        onPress={(e) => {
+          const { locationX, locationY } = e.nativeEvent
+          rewindOnDoubleTap(locationX, locationY)
+        }}
+      >
+        <Video
+          ref={playerRef}
+          source={{ uri: uri }}
+          onBuffer={() => console.log(1)}
+          onError={() => console.log(1)}
+          style={styles.player}
+          repeat
+          muted
+          controls
+          onProgress={(value) => setProgress(value.currentTime)}
+          rate={1}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: screenWidth * 0.9,
-    height: 200,
+    width: videoWidth,
+    height: videoHeight,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
