38da8c7be5f629bee59f8c8831521741b4794f9d
Gnarll <vadimfedorovich22@mail.ru>
2022-07-19 18:42:40 +0300

    delete useless code

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,58 +1,25 @@
-import React, { useRef, useState } from 'react'
-import {
-  Button,
-  Dimensions,
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native'
+import React, { useRef } from 'react'
+import { Dimensions, StyleSheet, View } from 'react-native'
 import Video from 'react-native-video'
-import { Slider } from '@miblanchard/react-native-slider'
 
 const { width: screenWidth } = Dimensions.get('screen')
 
 export const VideoPlayer = ({ uri }: { uri: string }) => {
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [isPaused, setIsPaused] = useState(true)
-  const [muted, setIsMuted] = useState(false)
-  const [volume, setVolume] = useState(0.5)
-  const playerRef = useRef(null)
-
-  const pauseHandler = () => setIsPaused((prevState) => !prevState)
-  const muteHandler = () => setIsMuted((prevState) => !prevState)
+  const playerRef = useRef<Video | null>(null)
 
   return (
-    <>
-      <TouchableOpacity onPress={pauseHandler} style={styles.container}>
-        <Video
-          ref={playerRef}
-          source={{ uri: uri }}
-          onBuffer={() => console.log(1)}
-          onError={() => console.log(1)}
-          style={styles.player}
-          paused={isPaused}
-          muted={muted}
-          repeat
-          volume={volume}
-          onReadyForDisplay={() => setIsLoaded(true)}
-        />
-        {isLoaded && isPaused && (
-          <Image
-            source={require('../../assets/pause-button.png')}
-            style={{ height: 30, width: 30, position: 'absolute' }}
-          />
-        )}
-      </TouchableOpacity>
-      <Button title="mute/unmute" onPress={muteHandler} />
-      <Slider
-        onValueChange={(value) => {
-          setVolume(value[0])
-        }}
-        value={volume}
+    <View style={styles.container}>
+      <Video
+        ref={playerRef}
+        source={{ uri: uri }}
+        onBuffer={() => console.log(1)}
+        onError={() => console.log(1)}
+        style={styles.player}
+        repeat
+        controls
+        rate={1}
       />
-    </>
+    </View>
   )
 }
 
