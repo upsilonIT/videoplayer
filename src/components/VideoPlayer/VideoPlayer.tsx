68b5e97e26d5900c01eefbb035a9082eed4c4424
Gnarll <vadimfedorovich22@mail.ru>
2022-07-20 17:44:47 +0300

    refactor rewind feature with gesture handler

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,64 +1,76 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
   Dimensions,
+  Platform,
   SafeAreaView,
   StyleSheet,
-  TouchableOpacity,
+  View,
 } from 'react-native'
+import {
+  GestureHandlerRootView,
+  State,
+  TapGestureHandler,
+  TapGestureHandlerStateChangeEvent,
+} from 'react-native-gesture-handler'
 import Video from 'react-native-video'
 
 const { width: screenWidth } = Dimensions.get('screen')
 
 const videoWidth = screenWidth * 0.9
 const videoHeight = 200
-
-const DOUBLE_PRESS_DELAY = 1000
+const rewindingViewWidth = videoWidth / 4.5
+const rewindingViewHeight = videoHeight / 2
+const rewindingViewHeightMargin = rewindingViewHeight / 2
 
 export const VideoPlayer = ({ uri }: { uri: string }) => {
   const playerRef = useRef<Video | null>(null)
+  const doubleTapRef = useRef<TapGestureHandler>(null)
   const [progress, setProgress] = useState(0)
 
-  const [lastPressed, setLastPressed] = useState(0)
+  const rewindLeft = () => playerRef?.current?.seek(progress - 15)
+  const rewindRight = () => playerRef?.current?.seek(progress + 15)
 
-  const rewindLeft = () => playerRef?.current?.seek(progress - 10)
-  const rewindRight = () => playerRef?.current?.seek(progress + 10)
-
-  const rewindOnDoubleTap = useCallback(
-    (locationX: number, locationY: number) => {
-      const time = new Date().getTime()
-
-      const delta = time - lastPressed
-      setLastPressed(time)
-      if (lastPressed && delta < DOUBLE_PRESS_DELAY) {
-        locationX < videoWidth / 2 ? rewindLeft() : rewindRight()
-      }
-    },
-    [lastPressed]
-  )
+  const rewindOnDoubleTap = (e: TapGestureHandlerStateChangeEvent) => {
+    playerRef.current?.context
+    if (
+      e.nativeEvent.state === State.ACTIVE &&
+      e.nativeEvent.y < rewindingViewHeight + rewindingViewHeightMargin &&
+      e.nativeEvent.y > rewindingViewHeightMargin
+    ) {
+      if (e.nativeEvent.x < rewindingViewWidth) rewindLeft()
+      if (e.nativeEvent.x > videoWidth - rewindingViewWidth) rewindRight()
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity
-        style={{ width: '100%', height: '100%', zIndex: 10 }}
-        activeOpacity={1}
-        onPress={(e) => {
-          const { locationX, locationY } = e.nativeEvent
-          rewindOnDoubleTap(locationX, locationY)
-        }}
-      >
-        <Video
-          ref={playerRef}
-          source={{ uri: uri }}
-          onBuffer={() => console.log(1)}
-          onError={() => console.log(1)}
-          style={styles.player}
-          repeat
-          muted
-          controls
-          onProgress={(value) => setProgress(value.currentTime)}
-          rate={1}
-        />
-      </TouchableOpacity>
+      <GestureHandlerRootView style={{ height: '100%', width: '100%' }}>
+        <TapGestureHandler
+          ref={doubleTapRef}
+          onHandlerStateChange={rewindOnDoubleTap}
+          numberOfTaps={2}
+        >
+          <View>
+            <Video
+              ref={playerRef}
+              source={{ uri: uri }}
+              onBuffer={() => console.log(1)}
+              onError={() => console.log(1)}
+              style={styles.player}
+              repeat
+              controls
+              onProgress={(value) => setProgress(value.currentTime)}
+              rate={1}
+            />
+            {Platform.OS === 'ios' && (
+              <>
+                <View style={styles.leftRewindingView} />
+                <View style={styles.rightRewindingView} />
+              </>
+            )}
+          </View>
+        </TapGestureHandler>
+      </GestureHandlerRootView>
     </SafeAreaView>
   )
 }
@@ -74,5 +86,19 @@ const styles = StyleSheet.create({
   player: {
     width: '100%',
     height: '100%',
+  },
+  leftRewindingView: {
+    width: rewindingViewWidth,
+    height: rewindingViewHeight,
+    position: 'absolute',
+    left: 0,
+    top: rewindingViewHeightMargin,
+  },
+  rightRewindingView: {
+    width: rewindingViewWidth,
+    height: rewindingViewHeight,
+    position: 'absolute',
+    right: 0,
+    top: rewindingViewHeightMargin,
   },
 })
